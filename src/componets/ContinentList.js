30637629727293604f090f4cee776dba0aee47a2
dickenzoh <dickenzoh@gmail.com>
2023-03-20 10:21:25 +0300

    added continent page

---
+++ src/componets/ContinentList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Text, StyleSheet, FlatList, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const ContinentList = ({ continents }) => {
+  return (
+    <>
+      <FlatList
+        data={continents}
+        keyExtractor={(_, index) => {
+          return index.toString();
+        }}
+        renderItem={({ item }) => <UserListItem continent={item} />}
+      />
+    </>
+  );
+};
+
+const UserListItem = ({ continent }) => {
+  return (
+    <View style={{ paddingHorizontal: 15 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          borderBottomWidth: 2,
+          borderBottomColor: "#ccc",
+        }}
+      >
+        <View style={{ paddingVertical: 15, paddingRight: 5 }}>
+          <MaterialCommunityIcons name="earth" color="green" size={40} />
+        </View>
+        <View style={styles.container}>
+          <Text>{continent["m:sName"][0]}</Text>
+          <Text>{continent["m:sCode"][0]}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 15,
+    alignItems: "flex-start",
+  },
+  baseText: {
+    fontWeight: "bold",
+  },
+  innerText: {
+    padding: 15,
+    color: "white",
+  },
+  thumbnail: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderColor: "green",
+    borderWidth: 3,
+  },
+});
+
+export default ContinentList;
--- src/navigation/stack-navigators/ContinentStackNavigator.js
+++ src/navigation/stack-navigators/ContinentStackNavigator.js
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Text } from "react-native";
+import { parseString } from "react-native-xml2js";
+
+import ContinentList from "../../componets/ContinentList";
 
 const ContinentStackNavigator = () => {
+  const [continents, setContinents] = useState([]);
+
+  // define the useEffect hook to fetch data when the component mounts
+  useEffect(() => {
+    // define the URL for the SOAP request
+    const url =
+      "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+
+    // define the SOAP request envelope
+    const envelope = `
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.oorsprong.org/websamples.countryinfo">
+           <soapenv:Header/>
+           <soapenv:Body>
+              <web:ListOfContinentsByName/>
+           </soapenv:Body>
+        </soapenv:Envelope>
+      `;
+
+    // define the headers for the SOAP request
+    const headers = new Headers({
+      "Content-Type": "text/xml;charset=UTF-8",
+      SOAPAction:
+        "http://www.oorsprong.org/websamples.countryinfo/ListOfContinentsByName",
+    });
+
+    // make the SOAP request using the fetch function
+    fetch(url, {
+      method: "POST",
+      headers,
+      body: envelope,
+    })
+      .then((response) => response.text())
+      .then((responseText) => {
+        console.log(responseText);
+        parseString(responseText, (error, result) => {
+          if (error) {
+            console.error(error);
+          } else {
+            // The result is a JavaScript object that you can use in your app
+            const continents =
+              result["soap:Envelope"]["soap:Body"][0][
+                "m:ListOfContinentsByNameResponse"
+              ][0]["m:ListOfContinentsByNameResult"][0]["m:tContinent"];
+            setContinents(continents);
+          }
+        });
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Continets screen</Text>
+    <View>
+      <View style={{ alignItems: "center", margin: 10 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+          Continents List
+        </Text>
+      </View>
+      <ContinentList continents={continents} />
     </View>
   );
 };
 
 export default ContinentStackNavigator;
-
-const styles = StyleSheet.create({});
